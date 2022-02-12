8a4d0eca6bb678a7087c9e8bfc7912a7f8a8757e
Jane <margjane@keemail.me>
2022-02-12 22:56:08 +0800

    slightly buff sniper rifles

--- gamemodes/terrortown/entities/weapons/weapon_tttwr_awp.lua
+++ gamemodes/terrortown/entities/weapons/weapon_tttwr_awp.lua
@@ -2,7 +2,7 @@ TTTWR.MakeSniper(SWEP,
 	"awp",
 	"awp",
 	{"weapons/awp/awp1.wav", 95},
-	50,
+	60,
 	60 / 40,
 	0,
 	7,
@@ -10,6 +10,6 @@ TTTWR.MakeSniper(SWEP,
 )
 
 
-SWEP.HeadshotMultiplier = 13 / 7 -- kills armoured players in one headshot + one bodyshot
+SWEP.HeadshotMultiplier = 1.65
 
 SWEP.ReloadTime = 4
